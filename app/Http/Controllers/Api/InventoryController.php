151a90dd1194413c53336dbb413bce58066504cd
Enth <enthusedfir@gmail.com>
2023-11-17 14:02:43 +0200

    Перенес Inventory в сервисы

--- app/Http/Controllers/Api/InventoryController.php
+++ app/Http/Controllers/Api/InventoryController.php
@@ -3,39 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Inventory;
-use App\Models\Warehouse;
-use Barryvdh\Debugbar\Facades\Debugbar;
+use App\Services\InventoryService;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
 
 class InventoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Response
-     */
-    public function index()
+
+    public function index(InventoryService $service)
     {
-        $org = Auth::user()->organisation;
-
-        $warehouses = $org->warehouses;
-
-        $inventories = $warehouses->map(function ($warehouse) {
-            return $warehouse->inventories;
-        });
-
-        return $inventories;
+        return $service->get();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @return Response
-     */
     public function store(CreateInventoryRequest $request)
     {
 
@@ -49,33 +27,14 @@ class InventoryController extends Controller
 //        ]);
     }
 
-    public function show($id)
+    public function show($id, InventoryService $service)
     {
-        $warehouse = Warehouse::find($id);
-
-        Debugbar::log($warehouse->inventories);
-
-        if (!$warehouse) {
-            return response(['message' => 'Not Found'], 404);
-        }
-
-
-        return $warehouse->inventories;
+        return $service->show($id);
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id, InventoryService $service)
     {
-        $inventory = Inventory::find($id);
-
-        if (!$inventory) return response(['message' => 'Cant find inventory'], 404);
-
-        $inventory->update([
-            'name' => $request->name,
-            'address' => $request->name,
-            'contact_info' => $request->name,
-        ]);
-
-        return response(['message' => 'Inventory changed successfully']);
+        return $service->update($id);
     }
 
     public function destroy(Request $request, int $id)
---
+++ app/Services/InventoryService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Inventory;
+use App\Models\Warehouse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class InventoryService
+{
+
+    public function get()
+    {
+        $org = Auth::user()->organisation;
+
+        $warehouses = $org->warehouses;
+
+        $inventories = $warehouses->map(function ($warehouse) {
+            return $warehouse->inventories;
+        });
+
+        return $inventories;
+    }
+
+    public function store(CreateInventoryRequest $request)
+    {
+
+//        $orgId = Auth::user()->organisation->id;
+//
+//        Outlet::create([
+//            'name' => $request->name,
+//            'address' => $request->address,
+//            'contact_info' => $request->contact_info,
+//            'organisation_id' => $orgId,
+//        ]);
+    }
+
+    public function show($id)
+    {
+        $warehouse = Warehouse::find($id);
+
+        if (!$warehouse) {
+            return response(['message' => 'Not Found'], 404);
+        }
+
+        return $warehouse->inventories;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inventory = Inventory::find($id);
+
+        if (!$inventory) return response(['message' => 'Cant find inventory'], 404);
+
+        $inventory->update([
+            'name' => $request->name,
+            'address' => $request->name,
+            'contact_info' => $request->name,
+        ]);
+
+        return response(['message' => 'Inventory changed successfully']);
+    }
+
+    public function destroy(Request $request, int $id)
+    {
+        //
+    }
+}
