a6ae6e898a1b2240eb11641922d53314a1d78057
Enth <enthusedfir@gmail.com>
2024-02-08 23:28:35 +0200

    Поправил размер кнопки Exit и убрал показ вкладки редактирования компании для сотрудников компании

--- resources/js/pages/Settings.vue
+++ resources/js/pages/Settings.vue
@@ -10,14 +10,16 @@ import ChangeEmailForm from "@/js/components/Forms/Auth/ChangeEmailForm.vue";
 defineEmits([...useDialogPluginComponent.emits])
 const {dialogRef, onDialogHide} = useDialogPluginComponent()
 
-const router = useRouter()
 const authStore = useAuthStore()
 
 const logoutModal = ref(false)
 
-onMounted(() => {
-    authStore.getUser()
+onMounted(async () => {
+    await authStore.getUser()
+
+    tab.value = authStore.role === 'Owner' ? 'company' : 'profile'
 })
+
 const tab = ref('company')
 const splitterModel = ref(120)
 </script>
@@ -30,7 +32,7 @@ const splitterModel = ref(120)
                         v-model="tab"
                         vertical
                     >
-                        <q-tab icon="apartment" label="Company" name="company"/>
+                        <q-tab v-if="authStore.role === 'Owner'" icon="apartment" label="Company" name="company"/>
                         <q-tab icon="account_circle" label="Profile" name="profile"/>
                         <q-tab icon="lock" label="Password" name="password"/>
                     </q-tabs>
@@ -45,13 +47,20 @@ const splitterModel = ref(120)
                         transition-prev="jump-down"
                         vertical
                     >
-                        <q-tab-panel class="pr-0" name="company">
+                        <q-tab-panel v-if="authStore.role === 'Owner'" class="pr-0" name="company">
                             <ChangeCompanyNameForm/>
                         </q-tab-panel>
 
                         <q-tab-panel class="pr-0" name="profile">
                             <ChangeNameForm/>
                             <ChangeEmailForm/>
+                            <div>
+                                <h2 class="text-bold">Role</h2>
+                                <q-input
+                                    v-model="authStore.role"
+                                    disable
+                                />
+                            </div>
                         </q-tab-panel>
 
                         <q-tab-panel class="pr-0" name="password">
@@ -59,14 +68,13 @@ const splitterModel = ref(120)
                         </q-tab-panel>
                     </q-tab-panels>
                 </template>
-
             </q-splitter>
         </q-card-section>
         <q-card-section class="grid grid-cols-4 gap-4 justify-items-end">
             <div></div>
             <div></div>
             <div></div>
-            <q-btn class="w-20" color="red" label="exit" @click="logoutModal = true"/>
+            <q-btn class="w-24" color="red" label="exit" @click="logoutModal = true"/>
         </q-card-section>
     </q-card>
 
