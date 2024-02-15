475e8718bf6949469e3746fb279b3ed9d99aba1d
Enth <enthusedfir@gmail.com>
2024-02-15 13:16:11 +0200

    Добавил обертку для форм

---
+++ resources/js/components/Forms/FormWrapper.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    loading: boolean
+}>()
+</script>
+
+<template>
+    <q-card class="q-dialog-plugin">
+        <transition
+            appear
+            enter-active-class="animated fadeIn"
+            leave-active-class="animated fadeOut"
+        >
+            <slot/>
+        </transition>
+        <q-inner-loading :showing="props.loading"/>
+    </q-card>
+</template>
